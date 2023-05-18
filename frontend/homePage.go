832b88a8a61c0566125cd2209c93653173024ec1
Nicklas185105 <nicklasbeyerlydersen@gmail.com>
2023-05-18 16:13:24 +0200

    changes to the homepage

--- frontend/homePage.go
+++ frontend/homePage.go
@@ -3,9 +3,9 @@ package frontend
 import "github.com/dtylman/gowd"
 
 const homeBody = `
-<div id="homeBody">
+<div id="homeBody" class="container-fluid text-center">
 	<div class="row">
-		<div class="col-md-12">
+		<div class="col">
 			<div class="jumbotron">
 				<h1>Clicker Game</h1>
 			</div>
@@ -13,6 +13,7 @@ const homeBody = `
 	</div>
 	<div class="row">
 		<div class="col-md-4 col-md-offset-4">
+			<button class="btn btn-primary btn-block" id="load">Load</button>
 			<button class="btn btn-primary btn-block" id="start">Start</button>
 		</div>
 	</div>
